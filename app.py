4d78223638a6344ecb39c1ad3b46707b3b9d23e2
kenin <stasstrahov16726@gmail.com>
2023-03-25 01:17:05 +0300

    Uploading images

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+import os
+import random
+
 from flask import Flask, render_template, request, redirect, url_for, session, abort, flash
-from flask_admin import Admin
+from flask_admin import Admin, form
 from flask_admin.contrib.sqla import ModelView
 from flask_login import LoginManager, login_user, logout_user, login_required
 from flask_sqlalchemy import SQLAlchemy
@@ -9,6 +12,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///store.db'
 app.config['SECRET_KEY'] = 'super secret key'
+app.config['STORAGE'] = 'static/products_img/'
 login_manager = LoginManager()
 login_manager.init_app(app)
 
@@ -20,6 +24,8 @@ class Product(db.Model):
     name = db.Column(db.String(100))
     price = db.Column(db.Float)
     description = db.Column(db.String(255))
+    collection = db.Column(db.String(255))
+    count_available = db.Column(db.Integer)
     image = db.Column(db.String(255))
 
 
@@ -33,8 +39,43 @@ class User(db.Model):
         return f'<User {self.name}>'
 
 
-class ProductView(ModelView):
-    form_columns = ('name', 'price', 'description', 'image')
+class StorageAdminModel(ModelView):
+    form_extra_fields = {
+        'file': form.FileUploadField('file')
+    }
+
+    def _change_path_data(self, _form):
+        try:
+            storage_file = _form.file.data
+
+            if storage_file is not None:
+                hash = random.getrandbits(128)
+                ext = storage_file.filename.split('.')[-1]
+                path = '%s.%s' % (hash, ext)
+
+                storage_file.save(
+                    os.path.join(app.config['STORAGE'], path)
+                )
+
+                _form.image.data = _form.name.data or storage_file.filename
+                _form.description.data = path
+
+                del _form.file
+
+        except Exception as ex:
+            pass
+
+        return _form
+
+    def edit_form(self, obj=None):
+        return self._change_path_data(
+            super(StorageAdminModel, self).edit_form(obj)
+        )
+
+    def create_form(self, obj=None):
+        return self._change_path_data(
+            super(StorageAdminModel, self).create_form(obj)
+        )
 
 
 class UserView(ModelView):
@@ -42,7 +83,7 @@ class UserView(ModelView):
 
 
 admin = Admin(app, name='Online Store Admin Panel')
-admin.add_view(ProductView(Product, db.session))
+admin.add_view(StorageAdminModel(Product, db.session))
 admin.add_view(UserView(User, db.session))
 
 
